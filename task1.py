07380823d3c343f7f6eb4a59cab640d60ea2732a
Aditya A <aragkar.adi@gmail.com>
2025-08-03 16:56:09 +0530

    2 and 3 subtask

--- task1.py
+++ task1.py
@@ -37,3 +37,99 @@ plt.axis('off')
 plt.tight_layout()
 plt.savefig("outputs/gray_comparison.png")
 plt.show()
+
+# ----------------------------------------------------------------------
+# Subtask 2: Grayscale to Black & White (Binary Image)
+# ----------------------------------------------------------------------
+
+# Manual Thresholding
+threshold_value = 128
+bw_manual = (gray_cv > threshold_value).astype(np.uint8) * 255
+
+# Built-in using OpenCV
+_, bw_cv = cv2.threshold(gray_cv, threshold_value, 255, cv2.THRESH_BINARY)
+
+# Alternative: Adaptive Threshold
+bw_adaptive = cv2.adaptiveThreshold(
+    gray_cv, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 3
+)
+
+_, bw_otsu = cv2.threshold(gray_cv, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+cv2.imwrite('outputs/bw_otsu.jpg', bw_otsu)
+
+
+# Save all binary images
+cv2.imwrite('outputs/bw_manual.jpg', bw_manual)
+cv2.imwrite('outputs/bw_cv.jpg', bw_cv)
+cv2.imwrite('outputs/bw_adaptive.jpg', bw_adaptive)
+
+# Display all
+plt.figure(figsize=(15,6))
+
+plt.subplot(1,5,1)
+plt.title("Grayscale")
+plt.imshow(gray_cv, cmap='gray')
+plt.axis('off')
+
+plt.subplot(1,5,2)
+plt.title("Manual (128)")
+plt.imshow(bw_manual, cmap='gray')
+plt.axis('off')
+
+plt.subplot(1,5,3)
+plt.title("cv2.threshold (128)")
+plt.imshow(bw_cv, cmap='gray')
+plt.axis('off')
+
+plt.subplot(1,5,4)
+plt.title("Adaptive (Gaussian)")
+plt.imshow(bw_adaptive, cmap='gray')
+plt.axis('off')
+
+plt.subplot(1,5,5)
+plt.title("Otsu")
+plt.imshow(bw_otsu, cmap='gray')
+plt.axis('off')
+
+plt.tight_layout()
+plt.savefig('outputs/bw_all_comparison.png')
+plt.show()
+
+# ----------------------------------------------------------------------
+# Subtask 3: Show One Plane at a Time (Others Set to Zero)
+# ----------------------------------------------------------------------
+
+r_plane_only = np.zeros_like(img_rgb)
+r_plane_only[:, :, 0] = img_rgb[:, :, 0]
+
+g_plane_only = np.zeros_like(img_rgb)
+g_plane_only[:, :, 1] = img_rgb[:, :, 1]
+
+b_plane_only = np.zeros_like(img_rgb)
+b_plane_only[:, :, 2] = img_rgb[:, :, 2]
+
+# Save each plane image
+cv2.imwrite('outputs/red_only.jpg', cv2.cvtColor(r_plane_only, cv2.COLOR_RGB2BGR))
+cv2.imwrite('outputs/green_only.jpg', cv2.cvtColor(g_plane_only, cv2.COLOR_RGB2BGR))
+cv2.imwrite('outputs/blue_only.jpg', cv2.cvtColor(b_plane_only, cv2.COLOR_RGB2BGR))
+
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 3, 1)
+plt.title("Red Plane")
+plt.imshow(r_plane_only)
+plt.axis('off')
+
+plt.subplot(1, 3, 2)
+plt.title("Green Plane")
+plt.imshow(g_plane_only)
+plt.axis('off')
+
+plt.subplot(1, 3, 3)
+plt.title("Blue Plane")
+plt.imshow(b_plane_only)
+plt.axis('off')
+
+plt.tight_layout()
+plt.savefig("outputs/planes_comparison.png")
+plt.show()
